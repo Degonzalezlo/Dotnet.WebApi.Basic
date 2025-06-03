1b8b454361be591e83d7c4646b833e011fceb2d6
Daniel <danielgonzalez.l@hotmail.com>
2025-06-03 06:55:33 -0500

    save changes project

--- MyVaccineAppSln/MyVaccine.WebApi/Program.cs
+++ MyVaccineAppSln/MyVaccine.WebApi/Program.cs
@@ -1,9 +1,25 @@
+using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+using MyVaccine.WebApi.Configurations;
+using MyVaccine.WebApi.Configurations.Validators;
+using MyVaccine.WebApi.Literals;
+using MyVaccine.WebApi.Models;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddControllers();  // (fv => fv.RegisterValidatorsFromAssemblies(Assembly.GetExecutingAssembly()));
+builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
+builder.Services.AddValidatorsFromAssemblyContaining<DependentsDtoValidator>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.SetDatabaseConfiguration();
+builder.Services.SetMyVaccineAuthConfigurations();
+builder.Services.SetDependencyInjection();
+builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
 
 var app = builder.Build();
 
@@ -16,29 +32,10 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+app.UseAuthentication();
+app.UseAuthorization();
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+app.MapControllers();
 
 app.Run();
 
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
